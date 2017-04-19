1632179cbd91a2614471cb333fc9ca39bc8653fb
zmichaels <zmichaels11@gmail.com>
2017-04-19 16:25:58 -0700

    - indirect and compute calls are now removed if GLES2 is selected.

--- gloop/draw_calls.cpp
+++ gloop/draw_calls.cpp
@@ -6,6 +6,8 @@
 
 #include "draw_calls.hpp"
 
+#include <type_traits>
+
 #include "exception/invalid_operation_exception.hpp"
 #include "gloop_throw.hpp"
 #include "wrapper/drawing_commands.hpp"
@@ -42,50 +44,101 @@ namespace gloop {
                 first,
                 count,
                 primitiveCount);
-    }   
+    }
+
+    namespace {
+#define GLEW 1
+#define GLES2 2
+#define GLES3 3
+#ifndef GL
+#define __GL_NOT_DEFINED
+#define GL 0
+#endif        
+
+        static constexpr bool supports_draw_indirect = (GL == GLEW || GL == GLES3);
+        static constexpr bool supports_compute_shaders = (GL == GLEW || GL == GLES3);
+
+#ifdef __GL_NOT_DEFINED
+#undef GL
+#undef __GL_NOT_DEFINED
+#endif
+
+#undef GLES3
+#undef GLES2
+#undef GLES1
+
+        template<bool isSupported = supports_draw_indirect, typename std::enable_if<isSupported, void * >::type = nullptr>
+        static inline void __drawArraysIndirect(
+                gloop::enum_t mode,
+                const void * indirect) {
+
+            wrapper::drawArraysIndirect(mode, indirect);
+        }
+
+        template<bool isSupported = supports_draw_indirect, typename std::enable_if<!isSupported, void * >::type = nullptr>
+        static inline void __drawArraysIndirect(...) {
+
+            gloop_throw(gloop::exception::invalid_operation_exception("Draw Indirect is not supported!"));
+        }
+
+        template<bool isSupported = supports_draw_indirect, typename std::enable_if<isSupported, void * >::type = nullptr>
+        static inline void __drawElementsIndirect(
+                gloop::enum_t mode,
+                gloop::enum_t type,
+                const void * indirect) {
+
+            wrapper::drawElementsIndirect(
+                    static_cast<gloop::enum_t> (mode),
+                    static_cast<gloop::enum_t> (type),
+                    indirect);
+        }
+
+        template<bool isSupported = supports_draw_indirect, typename std::enable_if<!isSupported, void * >::type = nullptr>
+        static inline void __drawElementsIndirect(...) {
+            gloop_throw(gloop::exception::invalid_operation_exception("drawElementsIndirect is not supported!"));
+        }
+
+        template<bool isSupported = supports_compute_shaders, typename std::enable_if<isSupported, void * >::type = nullptr>
+        static inline void __dispatchCompute(
+                gloop::uint_t numGroupsX,
+                gloop::uint_t numGroupsY,
+                gloop::uint_t numGroupsZ) {
+
+            wrapper::dispatchCompute(numGroupsX, numGroupsY, numGroupsZ);
+        }
+
+        template<bool isSupported = supports_compute_shaders, typename std::enable_if<!isSupported, void * >::type = nullptr>
+        static inline void __dispatchCompute(...) {
+            gloop_throw(gloop::exception::invalid_operation_exception("Compute Shaders are not supported!"));
+        }
+        
+        template<bool isSupported = supports_compute_shaders, typename std::enable_if<isSupported, void * >::type = nullptr>
+        static inline void __dispatchComputeIndirect(gloop::intptr_t indirect) {
+            wrapper::dispatchComputeIndirect(indirect);
+        }
+        
+        template<bool isSupported = supports_compute_shaders, typename std::enable_if<!isSupported, void * >::type = nullptr>
+        static inline void __dispatchComputeIndirect(...) {
+            gloop_throw(gloop::exception::invalid_operation_exception("Compute Shaders are not supported!"));
+        }
+    }
 
     void draw::arrays_indirect::draw() const {
-#define GLES2 2
-#if GL == GLES2
-        gloop_throw(gloop::exception::invalid_operation_exception("drawArraysIndirect is not supported!"));
-#else
-        wrapper::drawArraysIndirect(
-                static_cast<gloop::enum_t> (drawMode),
-                indirect);
-#endif
-#undef GLES2
+        __drawArraysIndirect<supports_draw_indirect>(static_cast<gloop::enum_t> (drawMode), indirect);
     }
 
     void draw::elements_indirect::draw() const {
-#define GLES2 2
-#if GL == GLES2
-        gloop_throw(gloop::exception::invalid_operation_exception("drawElementsIndirect is not supported!"));
-#else
-        wrapper::drawElementsIndirect(
+        __drawElementsIndirect(
                 static_cast<gloop::enum_t> (drawMode),
                 static_cast<gloop::enum_t> (type),
                 indirect);
-#endif
-#undef GLES2
     }
 
     void draw::compute::execute() const {
-#define GLES2 2
-#if GL == GLES2
-        gloop_throw(gloop::exception::invalid_operation_exception("dispatchCompute is not supported!"));
-#else
-        wrapper::dispatchCompute(numGroupsX, numGroupsY, numGroupsZ);
-#endif
-#undef GLES2
+        __dispatchCompute(numGroupsX, numGroupsY, numGroupsZ);
     }
 
     void draw::compute_indirect::execute() const {
-#define GLES2 2
-#if GL == GLES2
-        gloop_throw(gloop::exception::invalid_operation_exception("dispatchComputeIndirect is not supported!"));
-#else        
-        wrapper::dispatchComputeIndirect(indirect);
-#endif
-#undef GLES2
+        __dispatchComputeIndirect(indirect);
     }
 }
